3e9a0671da3eba20824c54dd8ec5c05d19e17c07
San-Moro <valentinacortese.dsg@gmail.com>
2022-12-07 16:17:27 +0100

    added the function constructor

--- Models/Movie.php
+++ Models/Movie.php
@@ -4,11 +4,23 @@ class Movie
 {
     public $name;
     public $directors;
-    public $duration;
     public $actors;
+    public $genre;
+    public $duration;
     public $release_date;
     public $original_language;
     public $vote;
 
-    
+    function __construct(String $name, String $directors, String $actors, String $genre, String $duration, String $release_date, String $original_language, Int $vote) {
+        $this->name = $name;
+        $this->directors = $directors;
+        $this->actors = $actors;
+        $this->genre = $genre;
+        $this->duration = $duration;
+        $this->release_date = $release_date;
+        $this->original_language = $original_language;
+        $this->vote = $vote;
+    }
+
+
 }
--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 require_once __DIR__ . "./Models/Movie.php";
 
-$matrix = new Movie();
-$matrix->name = "Matrix";
+$matrix = new Movie("Matrix", "Larry e Andy Wachowski", "Keanu Reeves-Laurence Fishburne-Carrie-Anne Moss", "cyberpunk", "130 minuti", "1999", "english", 5);
+
 var_dump($matrix);
+
+$goodfellas = new Movie("Goodfellas", "Martin Scorsese", "Ray Liotta - Robert De Niro - Joe Pesci", "biographical", "145 minuti", "1990", "english", 5);
+
+var_dump($goodfellas);
